14362235419928361fd169b1c4cc66d48bb2882f
AliSiddique <alisiddique10@hotmail.com>
2024-04-22 19:22:28 +0100

    SD

--- frontend/src/components/ui/EmptyState.tsx
+++ frontend/src/components/ui/EmptyState.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function EmptyState() {
     return (
         <div className="flex flex-col items-center justify-center h-full px-8">
-      <button
-        type="button"
+      <Link
+        href="/dashboard"
         className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
         <svg
@@ -20,7 +22,7 @@ export default function EmptyState() {
           />
         </svg>
         <span className="mt-2 block text-sm font-semibold text-gray-900">Label your images.</span>
-      </button>
+      </Link>
     </div>
     )
   }
