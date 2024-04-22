dc030cb8dbc7acf7a49f50df2c68bc0c05d650ec
AliSiddique <alisiddique10@hotmail.com>
2024-04-22 19:36:16 +0100

    AD

--- frontend/src/components/app/dashboard/LogView.tsx
+++ frontend/src/components/app/dashboard/LogView.tsx
@@ -93,6 +93,7 @@ export default function LogView({
                                                             name="project-name"
                                                             id="project-name"
                                                             defaultValue={log}
+                                                            disabled
                                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                                         />
                                                     </div>
@@ -111,6 +112,7 @@ export default function LogView({
                                                         <input
                                                             id="project-description"
                                                             name="project-description"
+                                                            disabled
                                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                                             defaultValue={url}
                                                         />
@@ -130,6 +132,7 @@ export default function LogView({
                                                         <input
                                                             id="project-description"
                                                             name="project-description"
+                                                            disabled
                                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                                             defaultValue={
                                                                 method
